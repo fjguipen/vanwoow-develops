810bea1a280622dd734bffa543d2cb72114d2c7f
fjguipen <fjguipen@gmail.com>
2019-04-14 14:23:04 +0200

    Added some example posts

--- data/posts.js
+++ data/posts.js
@@ -1,8 +1,47 @@
 const posts = [
     {
         id: 0,
+        title: "Entrada de ejemplo 1",
+        date: "8/2/19",
+        by:"JaviGP",
+        url:"",
+        body: 
+        `
+            <p>Párrafo uno</p>
+            <p>Párrafo dos</p>       
+            <p>Párrafo tres</p>       
+        `
+    },
+    {
+        id: 1,
+        title: "Entrada de ejemplo 2",
+        date: "15/2/19",
+        by:"JaviGP",
+        url:"",
+        body: 
+        `
+            <p>Párrafo uno</p>
+            <p>Párrafo dos</p>       
+            <p>Párrafo tres</p>       
+        `
+    },
+    {
+        id: 2,
+        title: "Entrada de ejemplo 3",
+        date: "18/2/19",
+        by:"JaviGP",
+        url:"",
+        body: 
+        `
+            <p>Párrafo uno</p>
+            <p>Párrafo dos</p>       
+            <p>Párrafo tres</p>       
+        `
+    },
+    {
+        id: 3,
         title: "¡Hola mundo..! ¡Hola vanwoow!",
-        date: "20/02/19",
+        date: "20/2/19",
         by:"JaviGP",
         url:"",
         body:
@@ -20,6 +59,22 @@ const posts = [
             <p>En esta página iré describiendo el proceso de construcción de la aplicación web de vanwoow. Describiré la arquitectura, hablaré sobre las distintas tecnologías que se aplican como <strong>React</strong> o <strong>Graphql</strong>, toma de decisiones... así como comentar y describir todas aquellas novedades y descubirmientos que aparezcan durante el proceso. En definitiva, quiero <strong>compartir la experiencia de la creación de la plataforma</strong>.</p>
             <br>
             <p>Así que si como yo, bebes 5 tazas de café al día, eres un freak de las nuevas tecnologías y te encanta piká código, <strong>¡te invito a que me acompañes y colabores abiertamente durante el desarollo!</strong></p>
+
+            <p>
+            Ejemplo:
+            <pre class="prettyprint lang-js">
+const Post = (post) => {
+    return (
+        \`
+        &lt;article&gt;
+            \${Title(post.title)}
+            \${DateC(post.date, post.by)}
+            \${Body(post.body)}
+        &lt;/article&gt;
+        \`
+    )
+}            </pre>
+            </p>
         `
     },
 ]
